fed1833758ba8559e722d2ea2ae46c9746a1660d
Enrico <enrico.milanese@whoi.edu>
2026-09-17 20:23:55 -0600

    actually working crocogallery pull patch

--- install.d/init.sh
+++ install.d/init.sh
@@ -57,10 +57,12 @@ if [[ "$INSTALL_CROCODASH" -eq 1 ]]; then
     git clone "$CROCODASH_GITHUB" "$CROCODASH_PATH"
     cd "$CROCODASH_PATH"
     git fetch --tags
-    git pull
     cd "$BASK_PATH"
     cd "$CROCODASH_PATH"
     git submodule update --init --recursive
+    cd "gallery"
+    git checkout main
+    git pull
     cd "$BASK_PATH"
     echo "CrocoDash downloaded."
 fi
